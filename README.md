f3ddedeae4c7d0ae75177107b81e5429a4d506a7
simplyatul <atulthosar@gmail.com>
2022-11-18 16:30:26 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Steps
+- Open STS 
+- File->New->Spring Starter Project
+- Enter details (like Group, Artifact, etc) 
+- Choose Maven and Java 8
+- Dependencies selected
+- Spring Data JPA
+- Spring Web
+- H2 Database
---
+++ src/test/java/com/simplyatul/springdatademo/SpringdatademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplyatul.springdatademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
