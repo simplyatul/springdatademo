8383d9b095a39e25f3b45b7c3badb5eb7c96564b
simplyatul <atulthosar@gmail.com>
2022-11-21 17:54:04 +0530

    some notes added

--- src/main/java/com/simplyatul/springdatademo/entities/Officer.java
+++ src/main/java/com/simplyatul/springdatademo/entities/Officer.java
@@ -7,14 +7,20 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
-@Table(name = "officers")
+@Entity // => This class is being mapped to DB table
+@Table(name = "officers") // Bec class name and table name is different 
 public class Officer {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    /*
+     * By default, enumes are saved as integer (1, 2, 3, etc)
+     * So in case you add an enum in bet, then all existing values would
+     * be messed up. So above annotation forces to save enums as their names
+     */
     private Rank rank;
     
     @Column(nullable = false, name = "name")
--- src/test/java/com/simplyatul/springdatademo/OfficerRepositoryTest.java
+++ src/test/java/com/simplyatul/springdatademo/OfficerRepositoryTest.java
@@ -1,12 +1,19 @@
 package com.simplyatul.springdatademo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +24,11 @@ import com.simplyatul.springdatademo.repos.OfficerRepository;
 @DataJpaTest
 @RunWith(SpringRunner.class)
 @Transactional
+/*
+ * Transactional => after every tests, transactions rolled back
+ * So you don't need to reset DB after every test
+ */
+
 class OfficerRepositoryTest {
     @Autowired
     @Qualifier("officerRepository")
@@ -26,18 +38,36 @@ class OfficerRepositoryTest {
      */
     private OfficerRepository repo;
     
+    @Autowired
+    private JdbcTemplate template;
+    /*
+     * way to pass already written SQL to DB and process the result
+     * return by DB
+     */
+    
+    
     @Test
+    //@Disabled
     public void testSave() {
         Officer officer = new Officer(Rank.ADMIRAL, "Soham");
         officer = repo.save(officer);
         System.out.println("Id: " + officer.getId());
         assertNotNull(officer.getId());
         assertEquals(Rank.ADMIRAL, officer.getRank());
+        System.out.println("Repo Count after insert: " + repo.count());
     }
 
     @Test
     public void count() throws Exception {
         System.out.println("Repo Count: " + repo.count());
         //assertEquals(2, repo.count());
+    }
+    
+    @Test
+    public void findAll() {
+        List<String> names = repo.findAll().stream()
+                .map(Officer::getName)
+                .collect(Collectors.toList());
+        //assertThat(names, containsInAnyOrder)
     }
 }
